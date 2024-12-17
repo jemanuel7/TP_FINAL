0b436dd345325027feb5c706f5b10641f401e435
Matias-Garay <Miguelgaray@uaa.edu.py>
2024-12-17 18:17:15 -0300

    Se agregan las pantallas de historial e informacion sobre el Usuario, se contemplan las validaciones necesarias para el correcto funcionamiento de los campos ya que ambas pantallas nos brindan información del usuario

---
+++ app/src/main/java/com/example/saludybienestar/screens/history/ActivityHistoryScreen.kt
@@ -0,0 +1,4 @@
+package com.example.saludybienestar.screens.history
+
+class ActivityHistoryScreen {
+}
---
+++ app/src/main/java/com/example/saludybienestar/screens/information/UserInfoViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.saludybienestar.screens.information
+
+class UserInfoViewModel {
+}
