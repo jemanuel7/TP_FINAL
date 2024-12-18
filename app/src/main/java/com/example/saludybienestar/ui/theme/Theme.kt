3ff41420fb9e5c180bf3e92efb4f0e821008f2f7
Adair <jaraadair@gmail.com>
2024-12-18 18:49:22 -0300

    se agregan cambios en la paleta de colores

--- app/src/main/java/com/example/saludybienestar/ui/theme/Theme.kt
+++ app/src/main/java/com/example/saludybienestar/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPastelGreen,
+    secondary = DarkPastelTeal,
+    tertiary = DarkPastelOlive
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LightPastelGreen,
+    secondary = LightPastelTeal,
+    tertiary = LightPastelOlive
 )
 
 @Composable
